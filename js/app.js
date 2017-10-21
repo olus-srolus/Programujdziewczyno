cd6d95e0372c6dc53e3d97bd920ebd3bb76fd115
olus-srolus <ale.godlewska@gmail.com>
2017-10-21 15:59:21 +0200

    commit2

---
+++ js/app.js
@@ -0,0 +1,22 @@
+console.log("działa!")
+
+var myName = 'Ola';
+
+console.log(myName);
+
+var myFriends = ['Kasia', 'Ola', 'Basia', 'Milena'];
+console.log(myFriends);
+
+for (var i = 0; i <= myFriends.length-1; i++) {
+console.log(myFriends[i]);
+}
+
+var a = 9;
+var b = 15;
+
+if (a>b) {
+    console.log("hurra!!!!!!");
+} else {
+    console.log(":C");
+};
+
